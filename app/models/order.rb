8b323873ed1a9b3c2e959496c497ab29863f0da7
Richard Casanova <ricardofer2412@hotmail.com>
2017-08-29 22:49:52 -0400

    add search menu

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,11 @@
 class Order < ApplicationRecord
-  belongs_to :user, :optional => true  
+  belongs_to :user, :optional => true
+
+  def self.search(search)
+    if search
+    where(['custumer_name LIKE ?',"%#{search}%"])
+  else
+      all
+    end
+  end
 end
