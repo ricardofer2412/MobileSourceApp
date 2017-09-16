2552b2b9ab6b40ac508ca53045ad62b18204dad1
Richard Casanova <ricardofer2412@hotmail.com>
2017-09-16 16:44:34 -0400

    Javascript Fix

--- app/assets/javascripts/orders.js
+++
@@ -1,6 +0,0 @@
-function update_price(){
-  var row = $(this).parents('.item.row');
-  var price = row.find('.cost').val() * row.find('.qty').val();
-  price = price.toFixed(2);
-  isNaN(price) ? row.find('.price').html('NAaN') : row.find('.price').html(price);
-}
